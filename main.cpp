3bc6271b219ff8727ad97c3b55c7012858245d9b
Sebastien Saez <monsieursaez@gmail.com>
2017-04-20 12:36:26 +0200

    added test program

--- main.cpp
+++ main.cpp
@@ -143,6 +143,7 @@ public:
       newNode.data = value;
       newNode.next = head;
       head = newNode;
+      nbElements++;
   }
   
 public:
@@ -171,7 +172,7 @@ public:
    */
   void pop_front() 
   {
-      
+      nbElements--;
   }
   
 public:
@@ -315,7 +316,41 @@ ostream& operator << (ostream& os, const LinkedList<T>& liste)
 }
 
 
-int main()
-{
-    return EXIT_SUCCESS;
+int main() {
+  
+  const int N = 10;
+  
+  cout << "Creation d'une liste de " << N << " entiers aléatoires \n";
+  LinkedList<int> liste;
+  for (unsigned i = 0; i < N; ++i) 
+  {
+    liste.push_front(rand()%100);
+  }
+  cout << "\n" << liste;
+  
+  cout << "\nModification de l'élément en tête à 42 \n";
+  liste.front() = 42;
+  cout << "\n" << liste;
+  
+  cout << "\nModification de l'élément en position " << N/2 << " à 24 \n";
+  liste.at(N/2) = 24;
+  cout << "\n" << liste;
+
+  cout << "\nSuppression de l'élément en position " << 2*N/3 << "\n";
+  liste.erase(2*N/3);
+  cout << "\n" << liste;
+  
+  cout << "\nInsertion de l'élément 421 en position 0\n";
+  liste.insert(421, 0);
+  cout << "\n" << liste;
+  
+  cout << "\nInsertion de l'élément 422 en position " << N/3 << "\n";
+  liste.insert(422, N/3);
+  cout << "\n" << liste;
+  
+  cout << "\nInsertion de l'élément 423 en dernière position\n";
+  liste.insert(423, liste.size());
+  cout << "\n" << liste;
+ 
+  cout << "\nDestruction de la liste \n";
 }
